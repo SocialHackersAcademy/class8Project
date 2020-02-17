1463eb064aba37a4a1eb83277e14d285d83d4d7a
Sohail <haqyarsohail@gmail.com>
2020-02-17 21:32:00 +0200

    Added the boilerplate database

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 // Middleware
 
 app.use(require("./Routes/post-routes"));
 
+// setting up the database
+
+mongoose.connect(
+  "mongodb://localhost/class_8_final",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => console.log("Connected to mongo db")
+);
 
 // PORT
 app.listen(process.env.PORT || 4000, () => {
