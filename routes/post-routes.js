11fda6c58fb6126934a9d25aeca5da7016e3589a
Sohail <haqyarsohail@gmail.com>
2020-02-24 16:18:47 +0200

    Added a post route that gets everything from the database

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const Post = require("../models/PostModel");
 router.get("/test", (req, res) => {
-  res.send("Healwejfoiawejf");
+  res.send("This is a test");
 });
+
+// Get all the posts from the Database
+router.get("/posts", async (req, res) => {
+  try {
+    const allPosts = await Post.find({});
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
