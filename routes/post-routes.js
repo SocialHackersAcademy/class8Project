0942aa249a0b70ca5ff5a0dbc355d23bfcbe8e4a
Sohail <haqyarsohail@gmail.com>
2020-02-24 16:25:04 +0200

    Added a route that works with authentication and gets all the users belonging to the authenticated orgnaization who made the request

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/PostModel");
+const passport = require("passport");
 
 router.get("/test", (req, res) => {
   res.send("Healwejfoiawejf");
 });
+
+// Get all the posts made by a specific user
+
+router.get(
+  "/posts/me",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const posts = await Post.find({ owner: req.user._id });
+      res.send(posts);
+    } catch (e) {
+      res.status(500).send();
+    }
+  }
+);
+
 module.exports = router;
