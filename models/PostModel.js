db34ca99b9b182dba4c81be1d8c1b09e0cc92070
Sohail <haqyarsohail@gmail.com>
2020-02-27 17:47:21 +0200

    Added one new branch merged in

--- models/PostModel.js
+++ models/PostModel.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-​
 const postSchema = new Schema(
   {
     subject: {
@@ -35,7 +34,6 @@ const postSchema = new Schema(
     timestamps:true
   }
 );
-​
 const Post = mongoose.model("Post", postSchema)
 
 module.exports = Post;
