8f6d153975429cc1aab20e85a42d2a5a8acd18f9
Sohail <haqyarsohail@gmail.com>
2020-02-27 17:15:33 +0200

    FIxed issues

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-<<<<<<< HEAD
 let express = require("express");
 let app = express();
 
 // sets port 8080 to default or unless otherwise specified in the environment
-app.set('port', process.env.PORT || 8080);
-=======
+app.set("port", process.env.PORT || 8080);
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
 // Middleware
->>>>>>> 435f27fefbae2bc2aa3c3a35acb4bef06fdfcdd2
 
 app.use(require("./routes/post-routes"));
 
